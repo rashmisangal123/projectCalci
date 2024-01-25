e95d25069619200ea4264b4fecd2a5ac0d77c3af
Rashmi <rashmisangal31@gmail.com>
2024-01-25 13:18:05 +0530

    program to find prime numbers using java

---
+++ primeno.java
@@ -0,0 +1,27 @@
+public class primeno {
+
+    public static void main(String[] args) {
+
+        int low = 20, high = 50;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
+
+    
+
+
